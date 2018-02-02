90c5d8f312e72017e7af0fe5d11aca5833f1a62c
cwjoshuak <jkuan1997@gmail.com>
2018-02-02 11:51:12 -0800

    Added a test comment on Line 39 on Character.h

--- Character/Character/Character.h
+++ Character/Character/Character.h
@@ -36,7 +36,7 @@ private:
         int xPos;
         int yPos;
     };
-    
+    // Josh added comment
     Location location; // instantiate an inner object from struct location
 public:
     /* Constructor*/
