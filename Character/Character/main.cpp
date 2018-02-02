a1dd7fab476cad01ef748d8149c72b7b7fbaabf0
khangvtran <vinhkhangtran09@gmail.com>
2018-02-02 09:56:11 -0800

    push main.cpp after editiing

--- Character/Character/main.cpp
+++ Character/Character/main.cpp
@@ -7,9 +7,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    cout << "test comitting git add" << endl;
     return 0;
 }
