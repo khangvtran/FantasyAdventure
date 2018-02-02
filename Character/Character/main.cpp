25e0660b7e22838c51d12e88250b5adb6537398c
khangvtran <vinhkhangtran09@gmail.com>
2018-02-02 09:45:29 -0800

    First Commit for Character Branch

---
+++ Character/Character/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Character
+//
+//  Created by Khang Tran on 2/2/18.
+//  Copyright © 2018 Khang Tran. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
