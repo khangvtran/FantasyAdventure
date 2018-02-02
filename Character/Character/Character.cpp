d35a6be64fdd299b11423d36eda2e868a7e0cdfc
khangvtran <vinhkhangtran09@gmail.com>
2018-02-02 11:41:41 -0800

    Base Character class

--- Character/Character/Character.cpp
+++ Character/Character/Character.cpp
@@ -7,3 +7,139 @@
 //
 
 #include "Character.h"
+
+/* Accessor */
+
+string Character::getName() const
+{
+    return name;
+}
+
+int Character::getHealth() const
+{
+    return health;
+}
+
+int Character::getStrength() const
+{
+    return strength;
+}
+
+int Character::getIntelligence() const
+{
+    return intelligence;
+}
+
+int Character::getLuck() const
+{
+    return luck;
+}
+
+bool Character::isAlive() const
+{
+    return alive;
+}
+
+int Character::getXPos() const
+{
+    return location.xPos;
+}
+
+int Character::getYPos() const
+{
+    return location.yPos;
+}
+
+
+
+/* Manipulator */
+void Character::setName(string Name)
+{
+    name = Name;
+}
+
+void Character::setHealth(int Health)
+{
+    health = Health;
+}
+
+void Character::setStrength(int Strength)
+{
+    strength = Strength;
+}
+
+
+void Character::setIntelligence(int Intelligence)
+{
+    intelligence = Intelligence;
+}
+
+void Character::setLuck(int Luck)
+{
+    luck = Luck;
+}
+
+void Character::die()
+{
+    alive = false;
+}
+
+void Character::setXPos(int X)
+{
+    location.xPos = X;
+}
+
+
+void Character::setYPos(int Y)
+{
+    location.yPos = Y;
+}
+
+
+/* Other operations */
+
+/*
+void Character::pickupItem(items item)
+{
+    
+}
+*/
+
+
+
+/*
+ void character::dropItem(items item)
+ {
+ 
+ }
+ */
+
+
+
+/*
+ void Character::useItem(items item)
+ {
+ 
+ }
+ 
+ */
+
+
+void move (char* moveCommand)
+{
+    cout << "Calling move Command" << endl;
+    // Ask Anna about the map
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Character/Character/Character.h
+++ Character/Character/Character.h
@@ -17,9 +17,78 @@
 # include <cstdlib>
 using namespace std;
 
+# include "LinkedList.h"
 
-
+class Character
+{
+private:
+    string name;
+    int health;
+    int strength;
+    int intelligence;
+    int luck;
+    bool alive = true;
+    // List<items> itemList;
+    // set<equipments> equipmentSet;
+    
+    struct Location
+    {
+        int xPos;
+        int yPos;
+    };
+    Location location; // instantiate an inner object from struct location
+public:
+    /* Constructor*/
+    Character(string Name, int row, int col) : name(Name)
+    {
+        setXPos(row-1);
+        setYPos(rand() % col);
+    }
+    
+    /* Destructor */
+    
+    
+    /* Accessor */
+    string getName() const;
+    int getHealth() const;
+    int getStrength() const;
+    int getIntelligence() const;
+    int getLuck() const;
+    bool isAlive() const;
+    int getXPos() const;
+    int getYPos() const;
+    
+    
+    /* Manipulator */
+    void setName(string Name);
+    void setHealth(int Health);
+    void setStrength(int Strength);
+    void setIntelligence(int Intelligence);
+    void setLuck(int Luck);
+    void die();
+    void setXPos(int X);
+    void setYPos(int Y);
+    
+    
+    /* Other operations */
+    
+    // void pickupItem(items item);
+    // void dropItem(items item)
+    // void useItem(items item);
+    // virtual void swapEquipment(equipments equipment) = 0;
+    // void attack(monsters monster) = 0;
+    virtual void useSpecialAbility() = 0;
+    void move(char * moveCommand);
+};
 
 
 
 #endif /* Character_hpp */
+
+
+
+
+
+
+
+
