d5915b1d28b8c22c20f47ab6df28db34cd7a6ae4
cwjoshuak <jkuan1997@gmail.com>
2018-02-02 10:03:02 -0800

    pushing main test2

---
+++ Character/Character/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Character
+//
+//  Created by Joshua Kuan on 02/02/2018.
+//  Copyright © 2018 Joshua Kuan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
