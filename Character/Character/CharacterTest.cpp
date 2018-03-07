10aeef6f6bb767fa2dc4f7b3b67ed034da8dd9f4
cwjoshuak <jkuan1997@gmail.com>
2018-03-06 19:45:44 -0800

    Game should be able to reset itself
    Infinite game!

--- Character/Character/CharacterTest.cpp
+++ Character/Character/CharacterTest.cpp
@@ -32,27 +32,33 @@ const string commands[] = {"move", "attack", "drop", "pickup", "activate", "use"
 const int numCommands = 8;
 const string INTRO = "AdventureGame \n\nWelcome to the phantasy adventure game! In search for riches and personal glory you have arrived at this\ndark and abandoned dungeon full of dragons and other creatures that lurk from around all corners ready to attack you and stall your journey for greatness. To find the treasure you will have to navigate through a labyrinth and slay monsters. Along the way you will find useful hints that will guide you toward the room with the treasure as well as maps that will show you your location in relation to the treasure room. You will collect items that will help you recover, kill monsters, and move closer to your goal. \n\nYou have 3 lives. Use them wisely!\n\n";
 bool isValidCommand(const string& command);
-void doCommand(const string& command, Character &c) throw(const char*);
+void doCommand(const string& command, Character* c) throw(const char*, AdventureErrors::InvalidMove, AdventureErrors::MissingObject, AdventureErrors::CharacterDeath);
 
-int main(void )
+int main(void)
 {
+    bool replay = true;
+    bool restart = true;
     srand(time(0));
-    cout << INTRO;
-    
-    //Create a Dungeon
-    Dungeon dungeon(ROWS, COLS);
-    
-    cout << "Enter a name: ";
-    string name;
-    getline(cin, name);
-    
-    //Create a Character
-    Character c(name, ROWS, COLS, dungeon);
-    
-    //Enter
+    Dungeon* dungeon = nullptr;
+    Character* c = nullptr;
     string command;
     do {
-        
+        if(restart)
+        {
+            cout << INTRO;
+            
+            //Create a Dungeon
+             dungeon = new Dungeon(ROWS, COLS);
+            
+            cout << "Enter a name: ";
+            string name;
+            getline(cin, name);
+            
+            //Create a Character
+            c = new Character(name, ROWS, COLS, *dungeon);
+            restart = false;
+        }
+        // Enter
         cout << "Enter command: ";
         getline(cin, command);
         try {
@@ -60,18 +66,49 @@ int main(void )
             if(isValidCommand(command))
                 doCommand(command, c);
             else
-                cout << "that wasn't a valid command" << endl;
-            cout << endl;
-            cout << "X: " << c.getRowPos() << " Y: " << c.getColPos() << endl;
-            cin.clear();
-            //dungeon.printMap(c.getXPos(), c.getYPos());
-            //c.printRoom();
+                cerr << "that wasn't a valid command" << endl;
             
-        } catch (const char* exception) {
+            cout << endl;
+            cout << "X: " << c->getRowPos() << " Y: " << c->getColPos() << endl;
+            
+        } catch (const char* &exception) {
             cerr << exception << endl;
-            cin.clear();
         }
-    } while(true);
+        catch (AdventureErrors::InvalidMove &err)
+        {
+            cerr << err.what() << endl;
+        }
+        catch(AdventureErrors::MissingObject &err)
+        {
+            cerr << err.what() << endl;
+        }
+        catch(AdventureErrors::CharacterDeath &err)
+        {
+            cerr << err.what() << endl;
+            char again;
+            do
+            {
+                cout << "Did you wish to play again? (Y/N): ";
+                cin >> again;
+                if(again == 'n' || again == 'N')
+                {
+                    cout << "Goodbye " << c->getName() << "!" << endl;
+                    replay = false;
+                }
+                else
+                {
+                    restart = true;
+                    cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"; // 20 newlines?
+                }
+                
+            } while(!(again != 'y' || again != 'Y' || again != 'n' || again != 'N'));
+            cin.clear();
+            cin.ignore(10, '\n');
+            delete dungeon;
+            delete c;
+        }
+        cin.clear();
+    } while(replay);
     
     return 0;
 }
@@ -90,33 +127,33 @@ bool isValidCommand(const string& command)
     return false;
 }
 
-void doCommand(const string &command, Character &c) throw(const char*)
+void doCommand(const string &command, Character *c) throw(const char*, AdventureErrors::InvalidMove, AdventureErrors::MissingObject, AdventureErrors::CharacterDeath)
 {
     size_t spacePos = command.find(" ");
     string cmd = command.substr(0, spacePos);
     
     try {
         if(cmd == commands[0]) // move
-            c.move(command.substr(spacePos+1));
+            c->move(command.substr(spacePos+1));
         else if(cmd == commands[1]) // attack
-            c.attack();
+            c->attack();
         else if(cmd == commands[2]) // drop
-            c.dropItem(command.substr(spacePos+1));
+            c->dropItem(command.substr(spacePos+1));
         else if(cmd == commands[3]) // pickup
-            c.pickupItem(command.substr(spacePos+1));
+            c->pickupItem(command.substr(spacePos+1));
         else if(cmd == commands[4]) // activate
-            c.activate(command.substr(spacePos+1));
+            c->activate(command.substr(spacePos+1));
         else if(cmd == commands[5])
-            c.useItem(command.substr(spacePos+1));
+            c->useItem(command.substr(spacePos+1));
         else if(cmd == commands[6])
-            c.print();
+            c->print();
         else if(cmd.substr(0,1) == commands[numCommands-1]) // cheat
-            c.cheat(command.substr(1,spacePos-1), command.substr(spacePos+1)); // possible cheat commands, "/god" and "/tp x y" without quotes, x y should be ints. to be implemented -> print map, spawn items into inv/equipment
+            c->cheat(command.substr(1,spacePos-1), command.substr(spacePos+1)); // possible cheat commands, "/god" and "/tp x y" without quotes, x y should be ints. to be implemented -> print map, spawn items into inv/equipment
         
         else;
     }
-    catch(const char*) {
-        throw;
-    }
-    
+    catch(const char*) { throw; }
+    catch(AdventureErrors::InvalidMove) { throw; }
+    catch(AdventureErrors::MissingObject) { throw; }
+    catch(AdventureErrors::CharacterDeath) { throw; }
 }
