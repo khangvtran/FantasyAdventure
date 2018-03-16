7ee14bf4e653d5f5aaac0d1ed7230a99f4bdcb5c
cwjoshuak <jkuan1997@gmail.com>
2018-03-16 13:05:53 -0700

    Reorganization

--- Character/Character/Exceptions.cpp
+++ Character/Character/Exceptions.cpp
@@ -6,3 +6,41 @@
 
 #include "Exceptions.h"
 
+namespace AdventureErrors
+{
+    BoundaryError::BoundaryError(const char* arg) : std::range_error(arg) {}
+    const char* BoundaryError::what() const noexcept
+    {
+        return (std::range_error::what());
+    }
+    
+    FileOpenError::FileOpenError(const char* arg) : std::invalid_argument(arg) {}
+    const char* FileOpenError::what() const noexcept
+    {
+        static char temp[300];
+        strcpy(temp, "File ");
+        strcat(temp, std::invalid_argument::what());
+        strcat(temp, " cannot be opened.");
+        return (temp);
+    }
+    
+    InvalidMove::InvalidMove(const char* what_arg) : std::invalid_argument(what_arg) {}
+    const char* InvalidMove::what() const noexcept
+    {
+        return invalid_argument::what();
+    }
+    
+    MissingObject::MissingObject(const char* what_arg) : std::logic_error(what_arg) {}
+    const char*  MissingObject::what() const noexcept
+    {
+        return logic_error::what();
+    }
+    
+    CharacterDeath::CharacterDeath(const char* what_arg) : std::runtime_error(what_arg) {}
+    const char* CharacterDeath::what() const noexcept
+    {
+        return runtime_error::what();
+    }
+ 
+}
+
--- Character/Character/Exceptions.h
+++ Character/Character/Exceptions.h
@@ -10,62 +10,40 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstring>
-#include <cstring>
-//using namespace std;
 
 namespace AdventureErrors
 {
     class BoundaryError : public std::range_error
     {
      public:
-        BoundaryError(const char* arg) : std::range_error(arg) {}
-        virtual const char* what() const throw()
-        {
-            return (std::range_error::what());
-        }
+        BoundaryError(const char* arg);
+        virtual const char* what() const noexcept;
     };
 
     class FileOpenError : public std::invalid_argument
     {
      public:
-        FileOpenError(const char* arg) : std::invalid_argument(arg) {}
-        virtual const char* what() const throw()
-        {
-            static char temp[300];
-            strcpy(temp, "File ");
-            strcat(temp, std::invalid_argument::what());
-            strcat(temp, " cannot be opened.");
-            return (temp);
-        }
+        FileOpenError(const char* arg);
+        virtual const char* what() const noexcept;
     };
     class InvalidMove : public std::invalid_argument
     {
      public:
-        explicit InvalidMove(const char* what_arg) : std::invalid_argument(what_arg){ }
-        virtual const char* what() const noexcept
-        {
-            return invalid_argument::what();
-        }
+        explicit InvalidMove(const char* what_arg);
+        virtual const char* what() const noexcept;
     };
 
     class MissingObject : public std::logic_error
     {
      public:
-        explicit MissingObject(const char* what_arg) : std::logic_error(what_arg){ }
-        virtual const char* what() const noexcept
-        {
-            return logic_error::what();
-        }
+        explicit MissingObject(const char* what_arg);
+        virtual const char* what() const noexcept;
     };
     class CharacterDeath : public std::runtime_error
     {
      public:
-        explicit CharacterDeath(const char* what_arg) : std::runtime_error(what_arg){ }
-        virtual const char* what() const noexcept
-        {
-            return runtime_error::what();
-        }
-        
+        explicit CharacterDeath(const char* what_arg);
+        virtual const char* what() const noexcept;
     };
 }
 
